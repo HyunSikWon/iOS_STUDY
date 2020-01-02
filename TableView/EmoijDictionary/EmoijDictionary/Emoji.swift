d6636273316a4b33817d3a3d039c84e15023404d
hyunsik won <shik0601@gmail.com>
2020-01-02 15:19:57 +0900

    Table View 공부

---
+++ TableView/EmoijDictionary/EmoijDictionary/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  EmoijDictionary
+//
+//  Created by 원현식 on 2020/01/02.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//  model
+
+import Foundation
+
+struct Emoji {
+    var symbol : String
+    var name : String
+    var description : String
+    var usage : String
+    
+    init(symbol : String, name : String, description : String, usage : String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}

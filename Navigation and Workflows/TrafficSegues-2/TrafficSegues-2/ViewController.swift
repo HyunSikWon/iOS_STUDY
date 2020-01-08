2a5ad1958d65640fb6d4cfcdd69ebf426e795d82
hyunsik won <shik0601@gmail.com>
2020-01-08 15:17:42 +0900

    Navigation and Workflows 본문 실습-2

---
+++ Navigation and Workflows/TrafficSegues-2/TrafficSegues-2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TrafficSegues-2
+//
+//  Created by 원현식 on 2020/01/08.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var segueSwitch: UISwitch!
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+}
+

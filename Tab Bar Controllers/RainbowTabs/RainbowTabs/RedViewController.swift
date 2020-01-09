4aff57287387311e07b7e427ddf94b1e84ec92e8
hyunsik won <shik0601@gmail.com>
2020-01-09 14:52:49 +0900

    Tab bar controller 본문 실습

---
+++ Tab Bar Controllers/RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by 원현식 on 2020/01/09.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

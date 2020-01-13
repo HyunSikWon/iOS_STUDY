01009f5bc432a560c02665ecfb481a3fa929455f
hyunsik won <shik0601@gmail.com>
2020-01-13 16:16:17 +0900

    saving data - lab 적용

--- MVC/FavoriteAthlete/FavoriteAthlete/Controller/AthleteTableViewController.swift
+++ MVC/FavoriteAthlete/FavoriteAthlete/Controller/AthleteTableViewController.swift
@@ -10,33 +10,43 @@ import UIKit
 
 class AthleteTableViewController: UITableViewController {
     
-    var athletes: [Athlete] = [Athlete(name: "Hyunjin-Ryu", age: "32", team: "Blue Jays", league: "MLB"),
-                               Athlete(name: "Tyler Wilson", age: "30", team: "LG Twins", league: "KBO")]
-
+    var athletes: [Athlete] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = editButtonItem
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let lodedAthletes = Athlete.loadFromFile()
+        if lodedAthletes.count > 0 {
+            print("File loaded")
+            athletes = lodedAthletes
+        } else {
+            print("samples loaded")
+            athletes = Athlete.loadSampleAthletes()
+        }
+        
     }
-
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return athletes.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AthleteCell", for: indexPath)
         
         cell.textLabel?.text = athletes[indexPath.row].name
         cell.detailTextLabel?.text = athletes[indexPath.row].description
-
+        
         return cell
     }
     
@@ -47,6 +57,7 @@ class AthleteTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             athletes.remove(at: indexPath.row)
+            Athlete.saveToFiles(athletes: athletes)
             tableView.reloadData()
         }
     }
@@ -55,6 +66,7 @@ class AthleteTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let movedAthlete = athletes.remove(at: sourceIndexPath.row)
         athletes.insert(movedAthlete, at: destinationIndexPath.row)
+        Athlete.saveToFiles(athletes: athletes)
         tableView.reloadData()
     }
     
@@ -81,7 +93,7 @@ class AthleteTableViewController: UITableViewController {
             athletes.append(athlete)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
-         
+        Athlete.saveToFiles(athletes: athletes)
         
     }
     
@@ -89,6 +101,6 @@ class AthleteTableViewController: UITableViewController {
     
     
     
-
-
+    
+    
 }
--- MVC/FavoriteAthlete/FavoriteAthlete/Model/Athlete.swift
+++ MVC/FavoriteAthlete/FavoriteAthlete/Model/Athlete.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Athlete {
+struct Athlete: Codable {
     var name: String
     var age: String
     var team: String
@@ -17,5 +17,40 @@ struct Athlete {
     var description: String {
         return "\(age) years old, \(team) / \(league) "
     }
+    
+    static var archiveURL: URL {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        return documentDirectory.appendingPathComponent("athletes").appendingPathExtension("plist")
+    }
+    
+    static func saveToFiles(athletes: [Athlete]) {
+        let pListEncoder = PropertyListEncoder()
+        let encodedAthletes = try? pListEncoder.encode(athletes)
+        try? encodedAthletes?.write(to: Athlete.archiveURL)
+        
+    }
+    
+    static func loadFromFile() -> [Athlete] {
+        let pListDecoder = PropertyListDecoder()
+        var athletesArr: [Athlete] = []
+        
+        if let retrievedAthletesData = try? Data(contentsOf: Athlete.archiveURL),
+            let decodedAthletes = try? pListDecoder.decode(Array<Athlete>.self, from: retrievedAthletesData) {
+            athletesArr = decodedAthletes
+            
+        }
+        
+        return athletesArr
+        
+    }
+    
+    static func loadSampleAthletes() -> [Athlete]  {
+           let athletes: [Athlete] = [Athlete(name: "Hyunjin-Ryu", age: "32", team: "Blue Jays", league: "MLB"),
+                                      Athlete(name: "Tyler Wilson", age: "30", team: "LG Twins", league: "KBO")]
+           
+           return athletes
+       }
+       
     
 }

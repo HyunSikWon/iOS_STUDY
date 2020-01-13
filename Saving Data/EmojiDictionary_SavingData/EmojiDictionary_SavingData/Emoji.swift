3e652c7fa2d04885e68766c9ec4ee56d61899f48
hyunsik won <shik0601@gmail.com>
2020-01-13 16:25:57 +0900

    EmojiDictionary-SavingData 작업중

---
+++ Saving Data/EmojiDictionary_SavingData/EmojiDictionary_SavingData/Emoji.swift
@@ -0,0 +1,9 @@
+//
+//  Emoji.swift
+//  EmojiDictionary_SavingData
+//
+//  Created by 원현식 on 2020/01/13.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import Foundation
---
+++ Saving Data/EmojiDictionary_SavingData/EmojiDictionary_SavingData/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary_SavingData
+//
+//  Created by 원현식 on 2020/01/13.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

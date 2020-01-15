93e82d9ebe859e8ade581a514f2704c631a78494
hyunsik won <shik0601@gmail.com>
2020-01-15 17:27:32 +0900

    Working with the web:  Decoding JSON LAB 실습. 미완성.. 공부해서 이해하고 다시 할것.

--- Working with the web/iTunes Search.playground/Contents.swift
+++ Working with the web/iTunes Search.playground/Contents.swift
@@ -13,22 +13,90 @@ extension URL {
     }
 }
 
-let baseURL = URL(string: "https://itunes.apple.com/search")!
+// MARK: - 실습 1
+//let baseURL = URL(string: "https://itunes.apple.com/search")!
+//
+//let query: [String: String] = [
+//    "term" : "yerin+baek",
+//    "media": "music",
+//    "entity":"album",
+//
+//]
+//
+//let url = baseURL.withQueries(query)!
+//
+//let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//    if let data = data,
+//        let string = String(data: data, encoding: .utf8){
+//        print(string)
+//    }
+//    PlaygroundPage.current.finishExecution()
+//}
+//
+//task.resume()
+
+
+// MARK: - 실습 2
+struct StoreItem: Codable {
+    var kind: String
+    var artist: String
+    var track: String
+    var collection: String
+    
+    enum CodingKeys: String, CodingKey {
+        case kind
+        case artist = "artistName"
+        case track = "trackName"
+        case collection = "collectionName"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
+        kind.self = try valueContainer.decode(String.self, forKey: .kind)
+        artist.self = try valueContainer.decode(String.self, forKey: .artist)
+        track.self = try valueContainer.decode(String.self, forKey: .track)
+        collection.self = try valueContainer.decode(String.self, forKey: .collection)
+        
+    }
+}
+
+struct StoreItems: Codable {
+    let results: [StoreItem]
+}
+
 
 let query: [String: String] = [
     "term" : "yerin+baek",
     "media": "music",
-    "entity":"album"
+    "entity":"album",
 ]
 
-let url = baseURL.withQueries(query)!
+func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
+    let baseURL = URL(string: "https://itunes.apple.com/search?")!
 
-let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-    if let data = data,
-        let string = String(data: data, encoding: .utf8){
-        print(string)
+    guard let url = baseURL.withQueries(query) else {
+        completion(nil)
+        print("Unable to build URL with supplied queries")
+        return
+
     }
-    PlaygroundPage.current.finishExecution()
+
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let jsonDecoder = JSONDecoder()
+        if let data = data,
+            let storeItems = try? jsonDecoder.decode(StoreItems.self, from: data) {
+            completion(storeItems.results)
+        } else {
+            print("No data was returned, or data was not serialized.")
+            completion(nil)
+            return
+        }
+        PlaygroundPage.current.finishExecution()
+    }
+
+    task.resume()
 }
 
-task.resume()
+fetchItems(matching: query) { (storeItems) in
+   print(storeItems)
+}

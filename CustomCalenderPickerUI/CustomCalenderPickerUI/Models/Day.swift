0e7600c79e04011e24cd7b873f87d8aa8c89b565
hyunsik won <shik0601@gmail.com>
2021-01-13 15:18:34 +0900

    remove folder

---
+++ CustomCalenderPickerUI/CustomCalenderPickerUI/Models/Day.swift
@@ -0,0 +1,15 @@
+//
+//  Day.swift
+//  CustomCalenderPickerUI
+//
+//  Created by 원현식 on 2021/01/12.
+//
+
+import Foundation
+
+struct Day {
+  let date: Date
+  let number: String
+  let isSelected: Bool
+  let isWithinDisplayedMonth: Bool
+}
---
+++ CustomCalenderPickerUI/CustomCalenderPickerUI/Models/MonthMetadata.swift
@@ -0,0 +1,14 @@
+//
+//  MonthMetadata.swift
+//  CustomCalenderPickerUI
+//
+//  Created by 원현식 on 2021/01/13.
+//
+
+import Foundation
+
+struct MonthMetadata {
+  let numberOfDays: Int
+  let firstDay: Date
+  let firstDayWeekday: Int
+}
